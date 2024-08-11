01fb9804126dd48ef6082e032ed6fc747380e535
toffeelab <toffeelab.game@gmail.com>
2024-08-11 17:03:16 +0900

    ✨ feat : next-themes 추가 및 prettier 설정

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -10,12 +10,13 @@ import { ReactNode } from 'react';
 import { Chakra_Petch } from 'next/font/google';
 import { isGeneral } from '@kursor/react/helpers/is.general';
 import PlausibleProvider from 'next-plausible';
+import { ThemeProvider } from 'next-themes';
 
 const chakra = Chakra_Petch({ weight: '400', subsets: ['latin'] });
 
 export default async function AppLayout({ children }: { children: ReactNode }) {
   return (
-    <html className={interClass}>
+    <html suppressHydrationWarning className={interClass}>
       <head>
         <link
           rel="icon"
@@ -24,9 +25,11 @@ export default async function AppLayout({ children }: { children: ReactNode }) {
         />
       </head>
       <body className={chakra.className}>
-        <PlausibleProvider domain="kursor.com">
-          <LayoutContext>{children}</LayoutContext>
-        </PlausibleProvider>
+        <ThemeProvider>
+          <PlausibleProvider domain="kursor.com">
+            <LayoutContext>{children}</LayoutContext>
+          </PlausibleProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
