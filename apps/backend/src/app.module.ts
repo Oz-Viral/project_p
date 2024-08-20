6ebabce907cf17ff4125ad094830868dc662f9fa
ybw0619 <ybw0619@naver.com>
2024-08-21 05:10:45 +0900

    feat: 도커 빌드 성공

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,20 +1,29 @@
-import {Global, Module} from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 
-import {DatabaseModule} from "@kursor/nestjs-libraries/database/prisma/database.module";
-import {ApiModule} from "@kursor/backend/api/api.module";
-import {APP_GUARD} from "@nestjs/core";
-import {PoliciesGuard} from "@kursor/backend/services/auth/permissions/permissions.guard";
+import { DatabaseModule } from '@kursor/nestjs-libraries/database/prisma/database.module';
+import { ApiModule } from '@kursor/backend/api/api.module';
+import { APP_GUARD } from '@nestjs/core';
+import { PoliciesGuard } from '@kursor/backend/services/auth/permissions/permissions.guard';
+import { ConfigModule } from '@nestjs/config';
 
 @Global()
 @Module({
-  imports: [DatabaseModule, ApiModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    DatabaseModule,
+    ApiModule,
+  ],
   controllers: [],
-  providers: [{
-    provide: APP_GUARD,
-    useClass: PoliciesGuard
-  }],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: PoliciesGuard,
+    },
+  ],
   get exports() {
     return [...this.imports];
-  }
+  },
 })
 export class AppModule {}
